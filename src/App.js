c264467e51e0f79c7d6a5c1ae6b1914e6222bfc7
ahaf82 <a_h82@gmx.net>
2020-05-11 23:24:55 +0200

    Logs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
 const App = () => {
+  useEffect(() => {
+    // Initializes Materialize JS
+    M.AutoInit();
+  });
+
   return (
-    <div className="App">
-      MyApp
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
   );
 }
 
