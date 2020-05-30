11ea59f33f0906a0fc49bb876a0ffd1b9613a231
ahaf82 <a_h82@gmx.net>
2020-05-30 13:20:51 +0200

    Prepare for deploy

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use('/techs', require('./routes/techs'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder   
-    app.use(express.static('build'));
+    app.use(express.static('client/build'));
 
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     });
 
 
